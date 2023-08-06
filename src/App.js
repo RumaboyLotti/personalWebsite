2e36936903a1bacc18379a133e3a57c59f2c1b80
RumaboyLotti <Oladimeji.bada18@gmail.com>
2023-08-06 06:28:02 -0500

    fixed utf-16 saving format to utf-8

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Intro from "./components/Intro";
 import Experience from "./components/Experience";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import logo from './logo.svg';
+import Contact from "./components/Contact";
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
