26c8bb0dd72835e04bc92485fe76b74bc9879d80
AsoftLotti <AsoftLotti@outlook.com>
2023-08-22 23:02:12 -0500

    changed header to a fucntional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 
-class Header extends React.Component{
-    // const [value, setValue] = React.useState('one');
-    render(){
-        
-        const handleChange = (event, newValue) => {
-            setValue(newValue);
-       
-        return(
-            <Box sx={{ width: '100%' }}>
-                <Tabs 
-                    // value={value} 
-                    // onChange={handleChange}
-                    // textColor="secondary"
-                    // indicatorColor="secondary"
-                    // aria-label="secondary tabs example"
-                    >
-                    <Tab value="one" label="About" />
-                    <Tab value="two" label="Experience" />
-                    <Tab value="three" label="Projects" />
-                    <Tab value="four" label="Contact" />
-                </Tabs> 
-            </Box>
-        );
-         };
+function Header() {
+    const [value, setValue] = useState(0);
+    
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
     };
+    
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Tabs 
+                value={value} 
+                onChange={handleChange}
+                textColor="secondary"
+                indicatorColor="secondary"
+                aria-label="secondary tabs example"
+            >
+                <Tab value={0} label="About" />
+                <Tab value={1} label="Experience" />
+                <Tab value={2} label="Projects" />
+                <Tab value={3} label="Contact" />
+            </Tabs> 
+        </Box>
+    );
 }
+
 export default Header;
