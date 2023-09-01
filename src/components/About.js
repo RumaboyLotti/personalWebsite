77fc2b57acf5ef08f4c943d01fc72763e149563b
RumaboyLotti <Oladimeji.bada18@gmail.com>
2023-09-01 01:57:57 -0500

    fixed typos

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ render(){
             I am a software developer.
             Majored in Information System and Technology, With a minor in Computer Science at University of Missouri Saint Louis .
             I am a passionate and dedicated individual who is eager to learn and grow in the field of software development.
-            I Have also recently gotten a Raspberry PI if you have any project ideas let me know</p>
+            I Have also recently gotten a Raspberry PI, so i'm open to ideas.</p>
 {/* 
             <figure id="Me"> 
                 <img src="https://i.imgur.com/5XZQq8M.jpg" alt="Ladi Bada" />  
