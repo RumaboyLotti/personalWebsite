998b64d28ca1ee4bff69393103a3507087a235dd
RumaboyLotti <Oladimeji.bada18@gmail.com>
2023-09-08 13:58:30 -0500

    making chnages from reccomendations

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,28 +1,30 @@
 import React from "react";
 import "../styles/Experience.css";
 
-class Experience extends React.Component{
-render(){
-    return(
-        <div id="experience">
-            <h1>Experience</h1> 
-            <ul id="joblist">
-                <div id="card1">
-                    <li>Seo Tech </li>
-                    <p>Emphasis on Data Structures & Algorithms in Python, 
-                        Using Api’s to solve problems and impletent them into projects,
-                        Working with Databases to store and modify data</p>
-                    </div>
-                <div id="card2">
-                    <li>UMSL IT Technician </li>
-                    <p>Completed 300+ hours of rigorous computer science and software engineering training focused on full-stack development, data structures, and algorithms.
-                       Gained expertise in web application development by building projects using Python (Flask), HTML, CSS, and MySQL.    
-                       Collaborated in SCRUM-like teams to build projects, such as wordle implementation for newspaper website, password creator website.
-                    </p>
-                    </div>
-            </ul>
-                </div>
+class Experience extends React.Component {
+  render() {
+    return (
+      <div id="experience">
+        <h1>Experience</h1>
+        <ul id="joblist">
+          <div id="card1">
+            <li>Seo Tech </li>
+            <p>
+            Completed 300+ hours of rigorous computer science and software engineering training focused on full-stack development, data structures, and algorithms
+Gained expertise in web application development by building projects using Python (Flask), HTML, CSS, and MySQL
+Collaborated in SCRUM-like teams to build projects, such as a Wordle implementation for a newspaper website, and a password creator website.
+            </p>
+          </div>
+          <div id="card2">
+            <li>UMSL IT Technician </li>
+            <p>Troubleshooted and solved technical issues from user error in UMSL-related software to hardware issues with UMSL-owned computers.
+Effective team communication via Microsoft Teams and Outlook email.
+Analyzed, integrated, and managed to operate UMSL’s system software, including Canvas, MyView, MyHr, SPSS, etc.
+</p>
+          </div>
+        </ul>
+      </div>
     );
-}
+  }
 }
 export default Experience;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,57 +1,76 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import CardContent  from "@mui/material/CardContent";
+import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import GitHubIcon from '@mui/icons-material/GitHub';
+import GitHubIcon from "@mui/icons-material/GitHub";
 import Box from "@mui/material/Box";
 import "../styles/Projects.css";
 
-
-class Projects extends React.Component{
-
-render(){
+class Projects extends React.Component {
+  render() {
     const bull = (
-        <Box
+      <Box
         component="span"
-        sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)",boxshadow: "20px 120px 45px black "}}
-        >
-        </Box>
+        sx={{
+          display: "inline-block",
+          mx: "2px",
+          transform: "scale(0.8)",
+        }}
+      ></Box>
     );
-    return(
-        <div>
-            <h1 id="projects">Projects</h1>
-            <div id="cardholder">
-            <Card sx={{ maxWidth: 345, backgroundColor: "#FEA47F" }} centered id ="card">
-                <CardContent>
-                    <Typography sx={{ fontSize: 10, color: "#B33771" }}  gutterBottom>  
-                    <a href="https://github.com/RumaboyLotti/MixedM"><GitHubIcon sx={{ color: "#B33771"}}></GitHubIcon></a>
-                        <h1>Mixed Message</h1>   
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                        <h5>Uses JavaScript to randomize words and present an astrology message</h5>
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        <h6>Js, and HTML</h6>
-                    </Typography>
-                </CardContent>
-            </Card>   
-            <Card sx={{ maxWidth: 345, backgroundColor: "#FEA47F" }} centered id ="card2">
-                <CardContent>
-                    <Typography sx={{ fontSize: 10, color: "#B33771" }}  gutterBottom>
-                    <a href="https://github.com/RumaboyLotti/RedditClone"><GitHubIcon sx={{ color: "#B33771"}}></GitHubIcon></a> 
-                        <h1>Reddit Clone</h1>   
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                        <h5>Created a Reddit clone to gain front-end web development experience.</h5>
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        <h6>JavaScript, React (Redux), HTML, CSS</h6>
-                    </Typography>
-                </CardContent>
-            </Card>  
-            </div>
+    return (
+      <div>
+        <h1 id="projects">Projects</h1>
+        <div id="cardholder">
+          <Card
+            sx={{ maxWidth: 345, backgroundColor: "#FEA47F" }}
+            centered
+            id="card"
+          >
+            <CardContent>
+              <Typography sx={{ fontSize: 10, color: "#B33771" }} gutterBottom>
+                <a href="https://github.com/RumaboyLotti/MixedM">
+                  <GitHubIcon sx={{ color: "#B33771" }}></GitHubIcon>
+                </a>
+                <h1>Mixed Message</h1>
+              </Typography>
+              <Typography variant="h5" component="div">
+                <h5>
+                  Uses JavaScript to randomize words and present an astrology
+                  message
+                </h5>
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                <h6>Js, and HTML</h6>
+              </Typography>
+            </CardContent>
+          </Card>
+          <Card
+            sx={{ maxWidth: 345, backgroundColor: "#FEA47F" }}
+            centered
+            id="card2"
+          >
+            <CardContent>
+              <Typography sx={{ fontSize: 10, color: "#B33771" }} gutterBottom>
+                <a href="https://github.com/RumaboyLotti/RedditClone">
+                  <GitHubIcon sx={{ color: "#B33771" }}></GitHubIcon>
+                </a>
+                <h1>Reddit Clone</h1>
+              </Typography>
+              <Typography variant="h5" component="div">
+                <h5>
+                  Created a Reddit clone to gain front-end web development
+                  experience.
+                </h5>
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                <h6>Js, React (Redux), HTML, CSS</h6>
+              </Typography>
+            </CardContent>
+          </Card>
         </div>
+      </div>
     );
-}
+  }
 }
 export default Projects;
