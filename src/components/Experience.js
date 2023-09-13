0c92c91b95d31505bc3e7945f4fc5d54445ff1b4
Ladi Bada <46547545+RumaboyLotti@users.noreply.github.com>
2023-09-13 17:48:40 -0500

    starting changes from init display

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,16 +10,17 @@ class Experience extends React.Component {
           <div id="card1">
             <li>Seo Tech </li>
             <p>
-            Completed 300+ hours of rigorous computer science and software engineering training focused on full-stack development, data structures, and algorithms
-Gained expertise in web application development by building projects using Python (Flask), HTML, CSS, and MySQL
-Collaborated in SCRUM-like teams to build projects, such as a Wordle implementation for a newspaper website, and a password creator website.
+            Completed 300+ hours of rigorous computer science and software engineering training focused on full-stack development, data structures, and algorithms.
+            Gained expertise in web application development by building projects using Python, Flask, HTML, CSS, and MySQL.
+            Collaborated in SCRUM-like teams to build projects, such as a Wordle implementation for a newspaper website, and a password creator website.
             </p>
           </div>
           <div id="card2">
             <li>UMSL IT Technician </li>
-            <p>Troubleshooted and solved technical issues from user error in UMSL-related software to hardware issues with UMSL-owned computers.
-Effective team communication via Microsoft Teams and Outlook email.
-Analyzed, integrated, and managed to operate UMSL’s system software, including Canvas, MyView, MyHr, SPSS, etc.
+            <p>
+            Troubleshooted and solved technical issues from user error in UMSL-related software to hardware issues with UMSL-owned computers.
+            Effective team communication via Microsoft Teams and Outlook email.
+            Analyzed, integrated, and managed to operate UMSL’s system software, including Canvas, MyView, MyHr, SPSS, etc.
 </p>
           </div>
         </ul>
