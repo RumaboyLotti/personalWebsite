8b833c7d28380477f473118b2116c69ab44fa69d
Ladi Bada <46547545+RumaboyLotti@users.noreply.github.com>
2023-12-04 05:38:45 -0600

    updating css and page format

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,21 +2,24 @@ import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import PageviewRoundedIcon from '@mui/icons-material/PageviewRounded';
 import "../styles/Projects.css";
-
 class Projects extends React.Component {
   render() {
     return (
-      <div>
+      <section id="projects">
         <h1 id="projects">Projects</h1>
         <div id="cardholder">
           <Card sx={{ maxWidth: 345, backgroundColor: "#FEA47F" }} id="card">
             <CardContent>
               <a href="https://github.com/RumaboyLotti/MixedM">
-                <GitHubIcon sx={{ color: "#B33771" }}></GitHubIcon>
+                <GitHubIcon sx={{ color: "#B33771" , height: "25px" }}></GitHubIcon>
               </a>
+              <a href="https://rumaboylotti.github.io/MixedM/">
+                <PageviewRoundedIcon sx={{ color: "#B33771" , paddingLeft: "20px"}}></PageviewRoundedIcon>
+                </a>
               <p className="cardHeader">Mixed Message</p>
-              <p className="card-desc">
+              <p className="card-desc"> 
                 Uses JavaScript to randomize words and present an astrology
                 message
               </p>
@@ -37,7 +40,7 @@ class Projects extends React.Component {
             </CardContent>
           </Card>
         </div>
-      </div>
+      </section>
     );
   }
 }
