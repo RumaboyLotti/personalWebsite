e7b8115a9679174ddeaabd0eccfee3d5206522f4
Ladi Bada <46547545+RumaboyLotti@users.noreply.github.com>
2023-12-09 15:01:19 -0600

    brough over contact gui changes for the form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,19 +6,51 @@ import '../styles/Global.css';
 import "../styles/Contact.css";
 
 class Contact extends React.Component{
-render(){
-    return(
-        <div id="contact">
-            <div id="table">
-            <ul id="contact-list">
-                <li> <a href="https://www.linkedin.com/in/ladi-bada-2a2460225/"><LinkedInIcon sx={{ color: "#FEA47F"}}></LinkedInIcon></a></li>
-                <li> <a href="https://github.com/RumaboyLotti"><GitHubIcon sx={{ color: "#FEA47F"}}></GitHubIcon></a></li>
-                <li> <a href= "mailto:@dtyasinmax@gmail.com" > <EmailIcon sx={{ color: "#FEA47F"}}></EmailIcon></a></li>
-            </ul>
+    render() {
+        return (
+          <section id="contact">
+            <h1 id="contact-title"> Looking for me? </h1>
+            <div id="contact">
+              <form action="/success" method="POST" name="contact">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" placeholder="Name" />
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" placeholder="Email" />
+                <label for="message">Message</label>
+                <textarea
+                  type="textfield"
+                  name="message"
+                  id="message"
+                  placeholder="Enter your Message Here!"
+                />
+                <input type="submit" value="Blast Off" />
+              </form>
             </div>
-        </div>
-    );
-}
+            <aside id="table">
+              <ul id="contact-list">
+                <li>
+                  {" "}
+                  <a href="https://www.linkedin.com/in/ladi-bada-2a2460225/">
+                    <LinkedInIcon sx={{ color: "#FEA47F" }}></LinkedInIcon>
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a href="https://github.com/RumaboyLotti">
+                    <GitHubIcon sx={{ color: "#FEA47F" }}></GitHubIcon>
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a href="mailto:dtyasinmax@gmail.com">
+                    <EmailIcon sx={{ color: "#FEA47F" }}></EmailIcon>
+                  </a>
+                </li>
+              </ul>
+            </aside>
+          </section>
+        );
+      }
 
 }
 export default Contact;
