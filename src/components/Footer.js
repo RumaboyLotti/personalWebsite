312d629cc685783f160190451a8d61a285ee15e1
Ladi Bada <46547545+RumaboyLotti@users.noreply.github.com>
2023-12-21 20:15:19 -0600

    redid header styles and format

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ContactForm from "./ContactForm";
 import "../styles/Footer.css";
 
 export class Footer extends React.Component {
    render() {
      return (
        <footer>
+        <ContactForm />
          <p>© 2021 Rumaboy Lotti</p>
        </footer>
      );
